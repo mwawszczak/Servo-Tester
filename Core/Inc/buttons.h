12388bd2a32c87c01786a42ce5eb95093e9c686f
mwawszczak <michal@blackbullet.pl>
2026-08-18 13:40:52 +0200

    Buttons: added stucture for button state, poll/pressed functions refactored to reflect the change

--- Core/Inc/buttons.h
+++ Core/Inc/buttons.h
@@ -1,10 +1,18 @@
 #ifndef INC_BUTTONS_H_
 #define INC_BUTTONS_H_
 
+#define SW_DEBOUNCE_TIME 3
 #include <stdbool.h>
 #include "gpio.h"
 
-
+typedef struct {
+	GPIO_TypeDef *port;
+	uint16_t pin;
+	uint32_t time_ms;
+	bool state;		// state after debounce
+	bool prev_state;	// previous button state for edge detection
+	bool pressed;
+} button_state_t;
 
 typedef enum {
 	BTN_MODE,
@@ -12,6 +20,6 @@ typedef enum {
 	BTN_ARM
 }button_t;
 
-GPIO_PinState buttons_poll(void);
-
+bool button_poll(button_state_t *btn);
+bool button_pressed(button_state_t *btn);
 #endif /* INC_BUTTONS_H_ */
